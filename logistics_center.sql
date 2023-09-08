56da3a3ccc8fd488fd37ce1c0774a8e49825c0d8
77255521 <77255521@gitlab.com>
2023-09-08 15:53:15 +0800

    数据库更新

--- logistics_center.sql
+++ logistics_center.sql
@@ -16,12 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `logistics_center`
+-- Table structure for table `admin`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `logistics_center` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `account` varchar(255) NOT NULL COMMENT '管理员账号',
+  `password` varchar(255) NOT NULL COMMENT '管理员登录密码',
+  `name` varchar(255) NOT NULL COMMENT '管理员姓名',
+  `phone` varchar(255) NOT NULL COMMENT '管理员联系方式',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `logistics_center`;
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'yangkai','123456','杨楷','189****5927'),(2,'kangmingqi','12345','康明淇','139****5621'),(3,'yuqi','123456','鱼鳍','168****3358');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `car`
@@ -31,13 +50,23 @@ DROP TABLE IF EXISTS `car`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `car` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `status` int DEFAULT NULL,
-  `describe` varchar(255) DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '车牌号',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '车辆名',
+  `status` int NOT NULL COMMENT '车辆状态：1代表配送中、2代表空闲中、3代表维修中',
+  `describe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '车辆外观描述',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car`
+--
+
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+INSERT INTO `car` VALUES ('CAR001','车辆1',1,'红色轿车'),('CAR002','车辆2',2,'蓝色货车'),('CAR003','车辆3',3,'黑色卡车'),('CAR004','车辆4',1,'白色SUV'),('CAR005','车辆5',1,'绿色货车');
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `good`
@@ -47,14 +76,23 @@ DROP TABLE IF EXISTS `good`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `good` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `warehouse` varchar(255) DEFAULT NULL,
-  `prize` decimal(10,2) DEFAULT NULL,
-  `number` int DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '物品Id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '物品名',
+  `warehouse` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '物品存储在哪个仓库',
+  `number` int DEFAULT NULL COMMENT '物品数量',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `good`
+--
+
+LOCK TABLES `good` WRITE;
+/*!40000 ALTER TABLE `good` DISABLE KEYS */;
+INSERT INTO `good` VALUES ('GOOD001','电饭煲','仓库1',50),('GOOD002','显示器','仓库2',60),('GOOD003','电视机','仓库3',20),('GOOD004','电风扇','仓库1',80),('GOOD005','台式电脑','仓库2',70),('GOOD006','冰箱','仓库3',40);
+/*!40000 ALTER TABLE `good` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `order`
@@ -64,16 +102,29 @@ DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `order` (
-  `id` varchar(255) NOT NULL,
-  `user_id` varchar(255) DEFAULT NULL,
-  `good_id` varchar(255) DEFAULT NULL,
-  `car_id` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `destination` varchar(255) DEFAULT NULL,
-  `describe` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '订单编号',
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收件人姓名',
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收件人Id',
+  `good_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '物品Id',
+  `car_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '车辆Id',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收件人电话',
+  `destination` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收件地址',
+  `describe` int NOT NULL COMMENT '物流描述：0表示未发货、1表示配送还需一天、2表似乎配送还需两天......以此类推；最高不超过10',
+  `price` decimal(10,2) NOT NULL COMMENT '订单价格',
+  `count` int NOT NULL COMMENT '订单数量',
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES ('ORDER01','赵六','4','GOOD001','CAR001','44444444444','陕西省西安市临潼区',2,20.61,5),('ORDER02','张三','1','GOOD002','CAR002','11111111111','陕西省西安市经开区',1,30.60,10),('ORDER03','郑九','7','GOOD003','CAR003','77777777777','陕西省西安市碑林区',0,50.98,5),('ORDER04','张三','1','GOOD005','CAR005','11111111111','陕西省西安市经开区',2,35.80,5);
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -83,14 +134,27 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `status` int DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '用户Id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户姓名',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户联系电话',
+  `account` varchar(255) NOT NULL COMMENT '用户账号',
+  `password` varchar(255) NOT NULL COMMENT '账号密码',
+  `age` int DEFAULT NULL COMMENT '用户年龄',
+  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户性别',
+  `address` varchar(255) DEFAULT NULL COMMENT '用户住址',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'张三','11111111111','zhangsan','123456',20,'男','陕西省西安市经开区'),(2,'李四','22222222222','lisi','1234567',21,'男','陕西省西安市未央区'),(3,'王五','33333333333','wangwu','123456',19,'男','陕西省西安市浐灞区'),(4,'赵六','44444444444','zhaoliu','12345',20,'女','陕西省西安市临潼区'),(5,'钱七','55555555555','qianqi','123456',19,'女 ','陕西省西安市新城区'),(6,'吴八','66666666666','wuba','123456',23,'男','陕西省西安市莲湖区'),(7,'郑九','77777777777','zhengjiu','123456',22,'男','陕西省西安市碑林区');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `warehouse`
@@ -100,11 +164,22 @@ DROP TABLE IF EXISTS `warehouse`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `warehouse` (
-  `id` varchar(255) NOT NULL,
-  `capacity` int DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '仓库Id',
+  `capacity` int DEFAULT NULL COMMENT '仓库容量',
+  `storage` int DEFAULT NULL COMMENT '仓库目前存放数量',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `warehouse`
+--
+
+LOCK TABLES `warehouse` WRITE;
+/*!40000 ALTER TABLE `warehouse` DISABLE KEYS */;
+INSERT INTO `warehouse` VALUES ('仓库1',500,150),('仓库2',400,175),('仓库3',700,400);
+/*!40000 ALTER TABLE `warehouse` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -115,4 +190,4 @@ CREATE TABLE `warehouse` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-07 11:11:47
+-- Dump completed on 2023-09-08 15:21:40
